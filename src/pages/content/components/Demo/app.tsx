d110b35dc2e05f863aea97d56015c8a196189b4d
Patrick Tran <patrick.tran06@gmail.com>
2023-05-17 23:06:36 -0700

    query the backend on the popup script and send to content script

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -10,14 +10,14 @@ function YouTubeVideo(props: { videoId: string }) {
         width="560"
         height="315"
         src={videoUrl}
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
 }
-function TagSection(props: { tag: string }) {
+function TagSection(props: { tag: string; tagDetails: string }) {
   const tagDivStyles = {
     padding: "5px",
     margin: "5px",
@@ -34,51 +34,68 @@ function TagSection(props: { tag: string }) {
     <div style={tagDivStyles}>
       <span style={tagStyles}>#{props.tag}</span>
       <div style={{ margin: "15px" }}>
-        <div>
+        {props.tagDetails ? props.tagDetails : "No details available"}
+        {/* <div>
           Here are three easy breakfast recipes to try during the week! Merguez
           & fontina stuffed croissants, Spiced egg & spinach breakfast wrap &
           the classic Scotch egg.
-          {/* <YouTubeVideo videoId="d-XL05EYWrQ" />{" "} */}
         </div>
         <div>comment 2</div>
-        <div>comment 3</div>
+        <div>comment 3</div> */}
+        {/* <YouTubeVideo videoId="d-XL05EYWrQ" />{" "} */}
       </div>
     </div>
   );
 }
 export default function App() {
   const [tags, setTags] = useState([]); // ["breakfast", "lunch", "dinner"
-  useEffect(() => {
-    console.log("content view loaded");
-    setTags(["breakfast", "lunch", "dinner"]);
+  const [tagOverview, setTagOverview] = useState(null);
+  // useEffect(() => {
+  //   console.log("content view loaded");
+  //   setTags(["breakfast", "lunch", "dinner"]);
 
-    // fetch("http://10.10.245.96:8080/get_summarized_topics", {
-    console.log("BROK");
-    fetch("https://e480-136-24-74-186.ngrok-free.app", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ videoid: "2SORT6-Fv8s" }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        console.log("KALE");
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error("Error:", error);
-      });
+  //   // fetch("http://10.10.245.96:8080/get_summarized_topics", {
+  //   console.log("BROK");
+  //   fetch("https://e480-136-24-74-186.ngrok-free.app", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ videoid: "2SORT6-Fv8s" }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Handle the response data
+  //       console.log("KALE");
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       // Handle any errors
+  //       console.error("Error:", error);
+  //     });
+  // }, []);
+  useEffect(() => {
+    const handleMessage = (message) => {
+      console.log(message); // You can access the message object here
+      setTagOverview(message["result"]);
+      setTags(Object.keys(message["result"]));
+    };
+
+    chrome.runtime.onMessage.addListener(handleMessage);
+
+    return () => {
+      chrome.runtime.onMessage.removeListener(handleMessage);
+    };
   }, []);
 
   return (
     <div className="content-view" style={{ marginBottom: "1500px" }}>
       Open Debate-ly
       <div className="mb-2">
-        {tags.map((tag, index) => (
-          <TagSection tag={tag} key={index} />
+        {Object.keys(tagOverview || {}).map((tag, index) => (
+          <div key={index}>
+            <TagSection tag={tag} tagDetails={tagOverview[tag]} />
+          </div>
         ))}
       </div>
     </div>
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
+  const [factCheck, setFactCheck] = React.useState(null);
+  useEffect(() => {
+    console.log("content view loaded");
+    // fetch("http://10.10.245.96:8080/get_summarized_topics", {
+    console.log("BROK");
+    fetch("https://e480-136-24-74-186.ngrok-free.app/get_summarized_topics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ videoid: "2SORT6-Fv8s" }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log("KALE");
+        console.log(data);
+        console.log(data["result"]);
+        const result = JSON.parse(data["result"]);
+        console.log(result);
+        setFactCheck(data["result"]);
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            const currentUrl = tabs[0].url;
+            const response = {
+              result: result,
+              url: currentUrl,
+            };
+            // const message = { message: "hello world" };
+            // console.log("sending message");
+            // chrome.tabs.sendMessage(tabs[0].id, message);
+            chrome.tabs.sendMessage(tabs[0].id, response);
+          }
+        );
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <h1>I didn't say that!</h1>
+        {factCheck ? <div>{factCheck}</div> : <div>fact check loading...</div>}
       </header>
     </div>
   );
